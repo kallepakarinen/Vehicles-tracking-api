f0348ea66556887e95e59b9d8ea0a0509f184ed3
kallepakarinen <cuttu85@gmail.com>
2018-09-18 11:19:41 +0300

    Create vehicle model and rename controller.

---
+++ VehiclesTrackingApi/VehiclesTrackingApi/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehiclesTrackingApi.Models
+{
+    [Table("Vehicle")]
+    public class Vehicle
+    {
+        [Key]
+        public int id { get; set; }
+        public string registration { get; set; }
+        public string mark { get; set; }
+        public string acquisitionDay { get; set; }
+        public decimal price { get; set; }
+        public decimal resaleValue { get; set; }
+        public bool active { get; set; }
+    }
+}
