09266495339e5d866d08681909acdf9e6ea6a099
kallepakarinen <cuttu85@gmail.com>
2018-09-18 13:19:33 +0300

    Create vehicle repository and use GET method

---
+++ VehiclesTrackingApi/VehiclesTrackingApi/Repositories/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiclesTrackingApi.Models;
+
+namespace VehiclesTrackingApi.Repositories
+{
+    public interface IVehicleRepository
+    {
+        List<Vehicle> Get();
+    }
+}
