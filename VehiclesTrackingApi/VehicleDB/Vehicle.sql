dc313175d2e9cff98e081e74b99dd6fcef488925
kallepakarinen <cuttu85@gmail.com>
2018-09-18 10:30:24 +0300

    Create database

---
+++ VehiclesTrackingApi/VehicleDB/Vehicle.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Vehicle]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [registration] NCHAR(10) NULL, 
+    [mark] NVARCHAR(50) NOT NULL, 
+    [acquisitionDay] DATE NULL, 
+    [price] DECIMAL NULL, 
+    [resaleValue] DECIMAL NULL, 
+    [active] BIT NOT NULL 
+)
