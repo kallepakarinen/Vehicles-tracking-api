20c9836edf3dc0c1e8619da1564ed52ea9600127
kallepakarinen <cuttu85@gmail.com>
2018-09-18 13:21:36 +0300

    create vehicle service, add get method and adding services to Startup and put localconnections connection string  to appsetting

---
+++ VehiclesTrackingApi/VehiclesTrackingApi/Services/IVehicleService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiclesTrackingApi.Models;
+
+namespace VehiclesTrackingApi.Services
+{
+    public interface IVehicleService
+    {
+        List<Vehicle> GetVehicles();
+    }
+}
--- VehiclesTrackingApi/VehiclesTrackingApi/Startup.cs
+++ VehiclesTrackingApi/VehiclesTrackingApi/Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VehiclesTrackingApi.Repositories;
+using VehiclesTrackingApi.Services;
 
 namespace VehiclesTrackingApi
 {
@@ -23,6 +26,12 @@ namespace VehiclesTrackingApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddDbContext<Models.VehicledbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["VehiclesDbConnection"]);
+            });
             services.AddMvc();
         }
 
